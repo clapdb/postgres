07dd86ee7155d3400d591a8c19bb904aa5cf0fe6
hurricane1026 <hurricane1026@gmail.com>
2026-07-09 05:25:57 +0800

    pagestore: harden SLRU mirror backend drains

--- contrib/pagestore/pagestore_slru.c
+++ contrib/pagestore/pagestore_slru.c
@@ -72,6 +72,7 @@
 #include "access/xlog.h"
 #include "access/xlog_internal.h"
 #include "access/xlogrecovery.h"
+#include "executor/executor.h"
 #include "fmgr.h"
 #include "funcapi.h"
 #include "miscadmin.h"
@@ -79,6 +80,7 @@
 #include "storage/ipc.h"
 #include "storage/shmem.h"
 #include "storage/spin.h"
+#include "tcop/utility.h"
 #include "utils/builtins.h"
 #include "utils/guc.h"
 #include "utils/memutils.h"
@@ -92,6 +94,8 @@ static bool pagestore_slru_mirror = false;
 
 static bool ps_slru_mirror_enabled = false;
 static slru_page_write_hook_type prev_slru_page_write_hook = NULL;
+static ExecutorEnd_hook_type prev_ExecutorEnd_hook = NULL;
+static ProcessUtility_hook_type prev_ProcessUtility_hook = NULL;
 
 /* Per-op mailbox timeout and whole-drain budget; see the control mirror. */
 #define PS_SLRU_SHIP_TIMEOUT_MS		10000
@@ -397,7 +401,7 @@ ps_slru_try_reserve_version(uint32 obj, uint32 pageno, XLogRecPtr bound)
 		}
 		SpinLockRelease(&slot->mutex);
 	}
-	return false;
+	return true;
 }
 
 /*
@@ -498,6 +502,11 @@ ps_slru_stage(SlruDesc *ctl, uint32 obj, uint32 pageno, const char *page,
 					 * (group LSNs reset on eviction/reload) must not
 					 * un-fence bits the older image already carried.
 					 */
+					if (!ps_slru_try_reserve_version(obj, pageno, bound))
+					{
+						ps_slru_note_recapture(ctl, obj, pageno, bound);
+						return;
+					}
 					memcpy(p->image, page, BLCKSZ);
 					if (p->fence_lsn < fence_lsn)
 						p->fence_lsn = fence_lsn;
@@ -514,6 +523,11 @@ ps_slru_stage(SlruDesc *ctl, uint32 obj, uint32 pageno, const char *page,
 		{
 			PsSlruPending *p = &ps_slru_queue[free_slot];
 
+			if (!ps_slru_try_reserve_version(obj, pageno, bound))
+			{
+				ps_slru_note_recapture(ctl, obj, pageno, bound);
+				return;
+			}
 			p->used = true;
 			p->shipped = false;
 			p->ctl = ctl;
@@ -544,7 +558,7 @@ static void
 ps_slru_write_hook(SlruDesc *ctl, int64 pageno, const char *page,
 				   XLogRecPtr fence_lsn)
 {
-	uint32		obj;
+	uint32		obj = 0;
 	XLogRecPtr	now;
 
 	if (prev_slru_page_write_hook)
@@ -575,12 +589,6 @@ ps_slru_write_hook(SlruDesc *ctl, int64 pageno, const char *page,
 	now = ps_slru_now_lsn();
 	if (XLogRecPtrIsInvalid(fence_lsn))
 		fence_lsn = now;
-	if (!ps_slru_try_reserve_version(obj, (uint32) pageno, now))
-	{
-		ps_slru_note_recapture(ctl, obj, (uint32) pageno, now);
-		return;
-	}
-
 	ps_slru_stage(ctl, obj, (uint32) pageno, page, fence_lsn, now);
 }
 
@@ -1057,6 +1065,31 @@ pagestore_slru_mirror_drain(void)
 	ps_slru_drain();
 }
 
+static void
+ps_slru_executor_end(QueryDesc *queryDesc)
+{
+	if (prev_ExecutorEnd_hook)
+		prev_ExecutorEnd_hook(queryDesc);
+	else
+		standard_ExecutorEnd(queryDesc);
+	pagestore_slru_mirror_drain();
+}
+
+static void
+ps_slru_process_utility(PlannedStmt *pstmt, const char *queryString,
+						bool readOnlyTree, ProcessUtilityContext context,
+						ParamListInfo params, QueryEnvironment *queryEnv,
+						DestReceiver *dest, QueryCompletion *qc)
+{
+	if (prev_ProcessUtility_hook)
+		prev_ProcessUtility_hook(pstmt, queryString, readOnlyTree, context,
+								 params, queryEnv, dest, qc);
+	else
+		standard_ProcessUtility(pstmt, queryString, readOnlyTree, context,
+								params, queryEnv, dest, qc);
+	pagestore_slru_mirror_drain();
+}
+
 /* before_shmem_exit: last chance to ship what this process staged. */
 static void
 ps_slru_exit_drain(int code, Datum arg)
@@ -1130,7 +1163,7 @@ pagestore_slru_live_read_at(PG_FUNCTION_ARGS)
 	int32		pageno = PG_GETARG_INT32(1);
 	XLogRecPtr	lsn = PG_GETARG_LSN(2);
 	PageStoreRelKey key;
-	uint32		obj;
+	uint32		obj = 0;
 	char	   *out = palloc(BLCKSZ);
 	bytea	   *result;
 
@@ -1186,9 +1219,12 @@ pagestore_slru_mirror_init(bool localsvc_active)
 				(errmsg("pagestore.slru_mirror requires pagestore.backend = 'localsvc'")));
 
 	ps_slru_mirror_enabled = true;
-	ps_slru_register_exit_drain();
 	prev_slru_page_write_hook = slru_page_write_hook;
 	slru_page_write_hook = ps_slru_write_hook;
+	prev_ExecutorEnd_hook = ExecutorEnd_hook;
+	ExecutorEnd_hook = ps_slru_executor_end;
+	prev_ProcessUtility_hook = ProcessUtility_hook;
+	ProcessUtility_hook = ps_slru_process_utility;
 
 	/* the loss counter is shared: most SLRU flushing is the checkpointer's */
 	prev_slru_shmem_request_hook = shmem_request_hook;
