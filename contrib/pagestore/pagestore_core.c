ff3150a7b609db416e7af028520e6d2b25fc354f
hurricane1026 <hurricane1026@gmail.com>
2026-07-22 14:15:06 +0800

    pagestore: enable timed upload join declaration

--- contrib/pagestore/pagestore_core.c
+++ contrib/pagestore/pagestore_core.c
@@ -28,6 +28,9 @@
  *
  *-------------------------------------------------------------------------
  */
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
 #include <errno.h>
 #include <pthread.h>
 #include <stdint.h>
