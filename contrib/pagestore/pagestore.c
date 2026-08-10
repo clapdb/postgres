676fd937b61b74f3eb35d201d6b8b909cf7950bb
hurricane1026 <hurricane1026@gmail.com>
2026-08-10 17:40:34 +0800

    fix(pagestore): declare bootstrap manifest path locally

--- contrib/pagestore/pagestore.c
+++ contrib/pagestore/pagestore.c
@@ -9882,7 +9882,6 @@ pagestore_install_prepared_branch(PG_FUNCTION_ARGS)
 	XLogRecPtr	fork_lsn;
 	char	   *manifest_data;
 	char	   *target_manifest;
-	char		manifest_path[MAXPGPATH];
 	char		stage[MAXPGPATH];
 	uint32_t	target_new_tl;
 	uint32_t	target_parent_tl;
@@ -10010,6 +10009,7 @@ pagestore_install_prepared_branch_bootstrap(PG_FUNCTION_ARGS)
 	XLogRecPtr	fork_lsn;
 	char	   *manifest_data;
 	char	   *target_manifest;
+	char		manifest_path[MAXPGPATH];
 	char	   *artifact;
 	PagestoreBranchBootstrapHeader *header;
 	ControlFileData *control;
